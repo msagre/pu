9154b8209b3b6d09148745d873fa558c2a68896b
msagre <58403299+msagre@users.noreply.github.com>
2024-06-10 12:43:14 -0300

    .

--- cpu4fpga.vhd
+++ cpu4fpga.vhd
@@ -1,3 +1,12 @@
-entity cpu4fpga is
-  port (
-    clk
+library ieee;
+use iee.std_logic_1164.all;
+
+entity pu is
+    port (
+        clk     : in  std_logic;
+        address : out std_logic_vector);
+end;
+
+architecture def of pu is
+begin
+    process 
